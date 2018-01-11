cc186956fb976a156cfc70c8095a43c56c658f80
Noah Edward Hall <nhall@l-sf6g7g8wn-m.gap.com>
2018-01-11 09:42:54 -0800

    add snapshot test

--- src/containers/__tests__/app.js
+++ src/containers/__tests__/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import { shallow } from 'enzyme';
 import { AppContainer as App } from '../app';
@@ -5,13 +6,7 @@ import { AppContainer as App } from '../app';
 const dispatch = () => null;
 
 describe('<App />', () => {
-  it('renders <App /> container', () => {
-    const wrapper = shallow(<App dispatch={dispatch} />);
-    expect(wrapper.find('div').length).toBe(0);
-  });
-
-  it('renders <App /> container', () => {
-    const wrapper = shallow(<App dispatch={dispatch} loaded />);
-    expect(wrapper.find('div').length).toBe(1);
+  it('renders correctly', () => {
+    expect(shallow(<App dispatch={dispatch} loaded />)).toMatchSnapshot();
   });
 });
